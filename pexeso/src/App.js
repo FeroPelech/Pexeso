cf88299cac3fbe9c2cb42cd904f2f6ec7a25c632
Frantisek Pelech <feromiro23@gmail.com>
2023-09-22 13:09:57 +0200

    Created shuffled array with pictures as objects with uniq id

--- pexeso/src/App.js
+++ pexeso/src/App.js
@@ -1,10 +1,34 @@
 import "./App.css";
+import { useState } from "react";
+
+const images = [
+  { src: "/img/Bukayo.png" },
+  { src: "/img/Jesus.png" },
+  { src: "/img/BW.png" },
+  { src: "/img/Gabriel.png" },
+  { src: "/img/Wilo.png" },
+  { src: "/img/MO.png" },
+];
 
 function App() {
+  const [pictures, setPictures] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  const doubleImages = () => {
+    const doubledImages = [...images, ...images]
+      .sort(() => Math.random() - 0.5)
+      .map((image) => ({ ...image, id: Math.random() }));
+    setPictures(doubledImages);
+    setTurns(0);
+  };
+
+  console.log(pictures);
+  console.log(turns);
+
   return (
     <div className="App">
       <h1>Pexeso</h1>
-      <button>New Game</button>
+      <button onClick={doubleImages}>New Game</button>
     </div>
   );
 }
